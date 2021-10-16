f6a09ee587fe5f0714cc0c14c503b083f7a9fd9f
Vicheanak Vannavy <vicheanak@gmail.com>
2021-10-16 11:09:42 +0700

    add some conditions

--- script.sh
+++ script.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+#Delete output.mp4
+output="output.mp4"
+if [ -f "$file" ] ; then
+    rm "$file"
+fi
 
+#Create tmp_source
+[[ -d ./test ]] && rm -r ./test
 mkdir tmp_source
 
+#Convert to same codec
 FILES="./source/*"
 for f in $FILES; 
 do ffmpeg -i $f -s hd720 -r 30000/1001 -video_track_timescale 30k -c:a copy ./tmp_source/${f/"./source/"/""}; 
@@ -24,6 +32,7 @@ ffmpeg -y -i speed.mp4 -vf hflip flip.mp4
 # #Black background
 ffmpeg -y -i flip.mp4 -filter_complex "pad=1280:0:(ow-iw)/2" output.mp4
 
+#Delete junk files
 rm -rf tmp_source
 rm ./source/*
 rm merge.mp4
